dad3809956e1e28815ac0cef1ce0ef5160376b3c
Felix.lin <fly_81211@hotmail.com>
2021-06-20 13:59:00 +0800

    temp

--- src/app/modules/main/school/components/license/school-license-add.component.ts
+++ src/app/modules/main/school/components/license/school-license-add.component.ts
@@ -1,7 +1,9 @@
 import { Component, Injector } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { take } from 'rxjs/operators';
+import { forkJoin } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
 import { ComponentBase } from 'src/app/base/component.base';
+import { ProfileModel } from 'src/app/types';
 
 @Component({
     selector: 'app-school-license-add',
@@ -10,6 +12,7 @@ import { ComponentBase } from 'src/app/base/component.base';
 })
 export class SchoolLicenseAddComponent extends ComponentBase {
     public licenseForm: FormGroup;
+    public profile: ProfileModel = null;
 
     constructor(
         private injector: Injector,
@@ -17,16 +20,48 @@ export class SchoolLicenseAddComponent extends ComponentBase {
     ) {
         super(injector);
         this.licenseForm = this.formBuilder.group({
+            school:['', [Validators.required]],
             contract: ['', [Validators.required, this.addressValidator]],
             name: ['', [Validators.required]],
             content: ['', [Validators.required]]
         });
+        this.getProfile();
+
     }
 
+    public async getProfile(): Promise<void> {
+        const resume = await this.providerSvc.getResume(this.providerSvc.defaultAccount);
+        const countReq = [];
+        this.profile = new ProfileModel(resume);
+        if(this.profile){
+            await this.profile.setBasic();
+        }
+        if(this.profile.account){
+            console.log(this.profile);
+            countReq.push(this.providerSvc.executeMethod(resume.methods.getEducationCount().call()));
+            forkJoin(countReq).pipe(
+                switchMap(res => {
+                    this.profile.setCounts(res);
+                    return this.profile.setEducations();
+                }),
+                take(1)
+            ).subscribe(() => {
+                this.licenseForm = this.formBuilder.group({
+                    school:[this.profile.educations.items[0].schoolName, [Validators.required]],
+                    contract: ['', [Validators.required, this.addressValidator]],
+                    name: ['', [Validators.required]],
+                    content: ['', [Validators.required]],
+                    comment: ['', [Validators.required]],
+                    grade: ['', [Validators.required, Validators.pattern(/^(?:[1-9]?\d|100)$/)]]
+                });
+            });
+        }
+    } 
+
     public async addLicense(data: any): Promise<void> {
-        const resume = await this.providerSvc.getResume(data.contract);
+        const resume = await this.providerSvc.getResume(this.providerSvc.defaultAccount);
         this.providerSvc.executeMethod(
-            resume.methods.setLicense(data.name, data.content)
+            resume.methods.setLicense(data.name, data.content,0)
             .send({ from: this.providerSvc.defaultAccount })
         ).pipe(
             take(1)
